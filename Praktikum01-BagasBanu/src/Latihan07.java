80674160c0c7c1df06ca9608d36745d804d6fe43
BagasBanu <114279942+BagasBanu@users.noreply.github.com>
2022-09-24 14:21:16 +0700

    Add files via upload

---
+++ Praktikum01-BagasBanu/src/Latihan07.java
@@ -0,0 +1,12 @@
+public class Latihan07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nama = "Bagas Banuwasito";
+		
+		nama = nama.replaceAll("[a,i,u,e,o]", "X");
+		
+		System.out.println("Bagas Banuwasito ==> " + nama);
+	}
+
+}
---
+++ Praktikum01-BagasBanu/src/Latihan08.java
@@ -0,0 +1,23 @@
+public class Latihan08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String NamaLengkap, kelas;
+		int NPM, semester;
+		double IPK;
+		
+		NamaLengkap = "Bagas Banuwasito";
+		kelas = "TI-21-PA";
+		NPM = 212310026;
+		semester = 3;
+		IPK = 3.8;
+		
+		System.out.println("NPM 		 : " + NPM);
+		System.out.println("Nama Lengkap : " + NamaLengkap);
+		System.out.println("Nama Kelas   : " + kelas);
+		System.out.println("Semester     : " +semester);
+		System.out.println("IPK 	     : " + IPK);
+		
+	}
+
+}
